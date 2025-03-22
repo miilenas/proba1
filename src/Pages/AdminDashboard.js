f687dace74dd02ee188b08a18e6e8e47bca30db3
Milena Sretenovic <ms20210061@student.fon.bg.ac.rs>
2025-03-22 01:50:25 +0100

    Izmenjena LoginPage i  AdminDashboard

--- src/Pages/AdminDashboard.js
+++ src/Pages/AdminDashboard.js
@@ -16,55 +16,63 @@ const AdminDashboard = () => {
        <div className="col-md-3">
         
          <div
-            className="card add-category-card card-s"
+            className="card add-category-card card-l"
             onClick={() => {
               navigate("/admin/users")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "50px" }}
+            style={{ cursor: "pointer", textAlign: "center", padding: "40px" }}
           >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#28a745" }}>Manage users</h2>
+              <p style={{ margin: "5px 0" }}>-add new user</p>
+              <p style={{ margin: "5px 0" }}>-update user</p>
             </div>
          </div>
          </div>
 
          <div className="col-md-3">
       <div
-            className="card add-category-card card-s"
+            className="card add-category-card card-l"
             onClick={() => {
               navigate("/admin/accounts")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "50px" }}
+            style={{ cursor: "pointer", textAlign: "center", padding: "40px" }}
       >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#28a745" }}>Manage accounts</h2>
+              <p style={{ margin: "5px 0" }}>-add new account</p>
+              <p style={{ margin: "5px 0" }}>-delete account</p>
             </div>
             </div>
       </div>
       <div className="col-md-3">
       <div
-            className="card add-category-card card-s"
+            className="card add-category-card card-l"
             onClick={() => {
               navigate("/admin/category")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "50px" }}
+            style={{ cursor: "pointer", textAlign: "center", padding: "30px" }}
           >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#28a745" }}>Manage categories</h2>
+              <p style={{ margin: "5px 0" }}>-add new category</p>
+              <p style={{ margin: "5px 0" }}>-update category</p>
+              <p style={{ margin: "5px 0" }}>-delete category</p>
             </div>
       </div>
       </div>
 
       <div className="col-md-3">
       <div
-            className="card add-category-card card-s"
+            className="card add-category-card card-l"
             onClick={() => {
               navigate("/register")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "50px" }}
+            style={{ cursor: "pointer", textAlign: "center", padding: "40px" }}
           >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#28a745" }}>Manage employees</h2>
+              <p>-add new employee</p>
             </div>
       </div>
 
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import ErrorModal from "../Components/AlertModal";
+import AlertModal from "../Components/AlertModal";
 import Form from "../Components/Form";
 
 const Login = ({ onLoginSuccess }) => {
   const [userData, setUserData] = useState({ email: "", password: "" });
-  const [loginError, setLoginError] = useState("");
+ // const [loginError, setLoginError] = useState("");
   const navigate = useNavigate();
 
+  const [ErrorModal, setErrorModal]=useState(false);
+  const[Messages, setMessages]=useState(false);
+  const[Title, setTitle]=useState(false);
+  
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
@@ -24,7 +28,7 @@ const Login = ({ onLoginSuccess }) => {
   function handleLogin(e) {
     e.preventDefault();
     console.log("Login form submitted!");
-    setLoginError(null);
+   // setLoginError(null);
     axios
       .post("http://127.0.0.1:8000/api/login", userData)
       .then((res) => {
@@ -40,15 +44,27 @@ const Login = ({ onLoginSuccess }) => {
             onLoginSuccess(res.data.user_type);
             navigate("/", { replace: true });
           }
-        } else {
-          console.log("Login failed or no access token received.");
-        }
+        } 
       })
-      .catch((e) => {
-        console.log("Login error:", e.response?.data?.message || e.message);
+      .catch((error) => {
+       /* console.log("Login error:", e.response?.data?.message || e.message);
         setLoginError(
           e.response?.data?.message || "An unexpected error occurred."
-        );
+        );*/
+
+        if (error.response && error.response.data.errors) {
+          console.log(error);
+          const errorArray = typeof error.response.data.errors === "string" ?
+          [error.response.data.errors] : Object.values(error.response.data.errors).flat(); 
+          setMessages(errorArray);
+          setTitle("Error");
+        } else {
+          setMessages(["Unexpected error."]);
+        }
+        setErrorModal(true);
+        console.error("Error login:", error);
+
+
       });
   }
 
@@ -77,8 +93,16 @@ const Login = ({ onLoginSuccess }) => {
         <button type="submit" className="btn btn-primary mt-3">
           Login
         </button>
-        <ErrorModal message={loginError} />
+       
       </form>
+      {ErrorModal && Messages && (
+        <AlertModal
+          title={Title}
+          message={Messages}
+          onClose={() => setErrorModal(false)}
+          type={Title}
+        />
+      )}
     </div>
   );
 };
