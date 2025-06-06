632d05af56c58d4c231bc9bbcbf265db537ed548
Aleksandra Rakovic <aleksandra.rakovic21@gmail.com>
2025-06-06 16:39:42 +0200

    Ispravljen CSS u AdminDashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/NavBar";
 import Login from "./Pages/LoginPage";
--- src/Pages/AdminDashboard.js
+++ src/Pages/AdminDashboard.js
@@ -1,6 +1,4 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
-import Card from "../Components/Card";
 import "../CSS/Card.css"
 
 
@@ -10,7 +8,7 @@ const AdminDashboard = () => {
 
   <div className="container mt-5 pb-4">
       <div>
-         <h2 class="pb-5">Admin Dashboard</h2>
+         <h2 className="pb-5">Admin Dashboard</h2>
       </div>
 
       <div className="row">
@@ -21,12 +19,12 @@ const AdminDashboard = () => {
             onClick={() => {
               navigate("/admin/users")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "40px" }}
+            style={{ cursor: "pointer", textAlign: "center" }}
           >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#0056b3" }}>Manage users</h2>
-              <p style={{fontSize: "17px", margin: "5px 0" }}>-add new user</p>
-              <p style={{fontSize: "17px", margin: "5px 0" }}>-update user</p>
+              <p className="admin-paragraf">-add new user</p>
+              <p className="admin-paragraf">-update user</p>
             </div>
          </div>
          </div>
@@ -37,12 +35,12 @@ const AdminDashboard = () => {
             onClick={() => {
               navigate("/admin/accounts")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "40px" }}
-      >
+           style={{ cursor: "pointer", textAlign: "center" }}
+           >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#0056b3" }}>Manage accounts</h2>
-              <p style={{fontSize: "17px", margin: "5px 0" }}>-add new account</p>
-              <p style={{fontSize: "17px", margin: "5px 0" }}>-delete account</p>
+              <p className="admin-paragraf">-add new account</p>
+              <p className="admin-paragraf">-delete account</p>
             </div>
             </div>
       </div>
@@ -52,13 +50,13 @@ const AdminDashboard = () => {
             onClick={() => {
               navigate("/admin/category")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "30px" }}
-          >
+           style={{ cursor: "pointer", textAlign: "center" }}
+           >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#0056b3" }}>Manage categories</h2>
-              <p style={{fontSize: "17px", margin: "5px 0" }}>-add new category</p>
-              <p style={{fontSize: "17px", margin: "5px 0" }}>-update category</p>
-              <p style={{fontSize: "17px", margin: "5px 0" }}>-delete category</p>
+              <p className="admin-paragraf">-add new category</p>
+              <p className="admin-paragraf">-update category</p>
+              <p className="admin-paragraf">-delete category</p>
             </div>
       </div>
       </div>
@@ -69,12 +67,12 @@ const AdminDashboard = () => {
             onClick={() => {
               navigate("/register")
             }}
-            style={{ cursor: "pointer", textAlign: "center", padding: "40px" }}
-          >
+           style={{ cursor: "pointer", textAlign: "center" }}
+           >
             <div className="card-body">
               <h2 style={{ fontSize: "25px", color: "#0056b3" }}>Manage employees</h2>
               <br></br>
-              <p style={{ fontSize: "17px"}}>-add new employee</p>
+              <p className="admin-paragraf">-add employee</p>
             </div>
       </div>
 
